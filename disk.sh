8af1212a822448d4169abc661afe65929825de19
zhuji999 <67562506+zhuji9999@users.noreply.github.com>
2023-04-17 16:48:11 +0800

    Update disk.sh

--- disk.sh
+++ disk.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 设置环境变量
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin  
+export PATH  
 
 echo "##############################################################"  
 echo "#                                                           #"  
@@ -11,67 +14,65 @@ echo "#     能自动检测硬盘并将硬盘挂载到用户指定目录        
 echo "#     挂载后添加至fstab实现开机自动挂载                         #"
 echo "#                                                           #"   
 echo "#     使用方法:                                              #"    
-echo "#     1. 赋予脚本执行权限: chmod +x Linux_auto_disk.sh        #" 
-echo "#     2. 运行脚本: ./Linux_auto_disk.sh                      #"    
+echo "#     1. 赋予脚本执行权限: chmod +x disk.sh                   #" 
+echo "#     2. 运行脚本: ./disk.sh                                 #"    
 echo "#     3. 选择要挂载的硬盘                                     #"
 echo "#     4. 输入要挂载的目录                                     #"  
 echo "#     5. 选择是否检测挂载结果                                  #"   
 echo "#                                                           #"    
 echo "##############################################################"   
 
-# 检测操作系统类型        
-if grep -qi ubuntu /etc/issue; then
-    OS_TYPE=Ubuntu        
-    DISK_PATH=/dev/         
-elif grep -qi debian /etc/issue; then
-    OS_TYPE=Debian        
-    DISK_PATH=/dev/              
-elif grep -qi centos /etc/redhat-release; then
-    OS_TYPE=CentOS        
-    DISK_PATH=/dev/          
-fi   
-
-# 使用lsblk命令显示硬盘信息
-lsblk  
-
-# 检测是否有可用硬盘
-read -p "请输入要挂载的硬盘: " DISK_NAME   
-
-# 显示所选硬盘详细信息  
-disk_info=$(lsblk /dev/${DISK_NAME})   
-echo $disk_info
 
 # 获取输入的挂载目录
-read -p "请输入挂载目录: " MOUNT_POINT        
-
-# 遍历可用硬盘  
-echo "正在挂载 $DISK_NAME 到 ${MOUNT_POINT}......"
-if [[ $OS_TYPE == Ubuntu ]] || [[ $OS_TYPE == Debian ]]; then             
-    mkdir -p ${MOUNT_POINT}
-elif [[ $OS_TYPE == CentOS ]]; then             
-    mkdir ${MOUNT_POINT}  
+read -p "请输入挂载目录: " mount_point
+if [ -z "$mount_point" ]; then
+    echo "挂载目录不能为空!"
+    exit 1
 fi
-mount /dev/${DISK_NAME} ${MOUNT_POINT}
-echo "/dev/${DISK_NAME} 已挂载到 ${MOUNT_POINT}。"        
 
-# 是否检测挂载成功
-read -p "是否检测挂载是否成功?[y/n] " TEST_MOUNT     
-if [[ $TEST_MOUNT == "n" ]]; then
-    echo "跳过挂载检测......"        
-elif [[ $TEST_MOUNT == "y" ]]; then
-    if mountpoint -q ${MOUNT_POINT}; then               
-        echo "挂载成功。"
-    else
-        echo "挂载失败。"        
-    fi
-fi    
-
-# 添加到fstab,开机自动挂载    
-if [[ $OS_TYPE == Ubuntu ]] || [[ $OS_TYPE == Debian ]]; then              
-    FSTAB_FILE=/etc/fstab
-elif [[ $OS_TYPE == CentOS ]]; then     
-    FSTAB_FILE=/etc/fstab          
+# 检测是否有硬盘可以挂载  
+disk_info=`fdisk -l | grep '^磁盘'` 
+if [ -z "$disk_info" ]; then
+    echo "没有可以挂载的硬盘!"
+    exit 0
 fi
-echo "/dev/${DISK_NAME}  ${MOUNT_POINT}  auto  defaults  0  0" >> $FSTAB_FILE      
-echo "已添加到${FSTAB_FILE},开机将自动挂载。"
 
+# 挂载硬盘
+fdisk() {
+    for disk in `fdisk -l | awk '/^磁盘/{print $2}'`; do
+        partition=`fdisk -l /dev/$disk | grep '^/dev/' | awk '{print $1}'`
+        # 检测分区是否已挂载,如果已挂载提示并跳过
+        mounted=`df -h | grep $partition`
+        if [ ! -z "$mounted" ]; then
+            echo "$partition 已挂载!"
+            continue
+        fi 
+        # 创建挂载点目录
+        mkdir $mount_point
+        # 添加开机自动挂载信息
+        echo "$partition $mount_point 自动 默认值 0 0" >> /etc/fstab
+        # 挂载分区
+        mount -a
+        # 检测是否挂载成功 
+        while true; do 
+            read -p "检查挂载是否成功?(y/n) :" check
+            if [ "$check" = "y" ]; then
+                mounted_now=`df -h | grep $partition`
+                if [ ! -z "$mounted_now" ]; then  
+                    echo "$partition 挂载成功!"
+                else
+                    echo "挂载失败!" 
+                    umount $mount_point
+                    sed -i "/$partition/d" /etc/fstab
+                fi
+                break
+            elif [ "$check" = "n" ]; then 
+                break
+            else
+                echo "请输入 y 或 n!"
+            fi
+        done
+    done
+} 
+
+fdisk
